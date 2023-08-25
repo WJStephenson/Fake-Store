333ef315209a24074b351a9756b8486a85584c53
WJStephenson <willjosteph@gmail.com>
2023-08-25 16:02:34 +0100

    added heart icon to add/remove from cart

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,38 +1,40 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Card.css'
 import axios from 'axios'
-import { AiOutlineHeart as Heart } from 'react-icons/ai'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
 
 function Card({ product }) {
 
-    const { cart } = useContext(CartContext)
+    const { cart, addProduct, removeProduct } = useContext(CartContext)
 
     const [inBasket, setInBasket] = useState(false)
 
     useEffect(
-        ()=>{
+        () => {
             setInBasket(cart?.find(item => item.id === product.id))
         }, [cart]
     )
 
     return (
+        <div className='card-wrapper'>
+            <Link to={`details/${product.id}`} className='card-container'>
+                <img src={product.image} placeholder={product.title}></img>
+                <h3>{product.title}</h3>
+                <p>{product.category}</p>
+                <h3>${parseFloat(product.price).toFixed(2)}</h3>
+            </Link>
 
-        <Link to={`details/${product.id}`} className='card-container'>
-            <img src={product.image} placeholder={product.title}></img>
-            <h3>{product.title}</h3>
-            <p>{product.category}</p>
-            <h3>${parseFloat(product.price).toFixed(2)}</h3>
             <div className='heart-container'>
                 {
                     inBasket?
-                    <i className='heart-icon'><Heart style={{color: "red"}}/></i>
-                    :
-                    <i className='heart-icon'><Heart /></i>
+                        <i className='heart-icon' onClick={()=>removeProduct(product.id)}><AiFillHeart style={{ color: "red" }} /></i>
+                        :
+                        <i className='heart-icon' onClick={()=>addProduct(product)}><AiOutlineHeart /></i>
                 }
             </div>
-        </Link>
+        </div>
     )
 }
 
